c27d164e7c3b6ae78f6f90a103fcddf94eb3c848
jason <zahinjason220434@gmail.com>
2018-12-29 20:17:55 +0600

    Skeleton Updates

--- app/src/main/java/com/example/jason/qair/ApploginPage.java
+++ app/src/main/java/com/example/jason/qair/ApploginPage.java
@@ -6,17 +6,24 @@ import android.view.View;
 import android.widget.Button;
 
 public class ApploginPage extends Activity {
-    private Button SignUp;
+    private Button SignUp,LogIn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_applogin_page);
         SignUp = findViewById(R.id.Signup);
+        LogIn = findViewById(R.id.Login);
         SignUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 setContentView(R.layout.activity_sign_up);
+            }
+        });
+        LogIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setContentView(R.layout.activity_account_page);
             }
         });
     }
