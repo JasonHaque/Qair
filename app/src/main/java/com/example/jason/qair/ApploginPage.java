0399850cb650b6553d86f4543d227d3deeda7eb9
jason <zahinjason220434@gmail.com>
2018-12-29 20:06:10 +0600

    added SignUp skeleton and fire Base Dependency

--- app/src/main/java/com/example/jason/qair/ApploginPage.java
+++ app/src/main/java/com/example/jason/qair/ApploginPage.java
@@ -2,12 +2,23 @@ package com.example.jason.qair;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ApploginPage extends Activity {
+    private Button SignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_applogin_page);
+        SignUp = findViewById(R.id.Signup);
+        SignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setContentView(R.layout.activity_sign_up);
+            }
+        });
     }
+
 }
