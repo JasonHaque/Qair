bbddee616fa74a206d758408e6b20c87a5789ee7
jason <zahinjason220434@gmail.com>
2019-01-05 01:43:21 +0600

    Sign Up layout finalized

--- app/src/main/java/com/example/jason/qair/SignUp.java
+++ app/src/main/java/com/example/jason/qair/SignUp.java
@@ -2,12 +2,18 @@ package com.example.jason.qair;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
 
 public class SignUp extends Activity {
+    private CheckBox teacher_check,student_check;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
     }
+
+
 }
