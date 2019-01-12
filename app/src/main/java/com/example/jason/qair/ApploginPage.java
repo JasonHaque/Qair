4b946b94739f335ffd89feab9207425f3c0102a2
jason <zahinjason220434@gmail.com>
2019-01-12 16:38:17 +0600

    Sign Up bugs fixed

--- app/src/main/java/com/example/jason/qair/ApploginPage.java
+++ app/src/main/java/com/example/jason/qair/ApploginPage.java
@@ -1,6 +1,7 @@
 package com.example.jason.qair;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -17,7 +18,7 @@ public class ApploginPage extends Activity {
         SignUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                setContentView(R.layout.activity_sign_up);
+                startActivity(new Intent(ApploginPage.this,com.example.jason.qair.SignUp.class));
             }
         });
         LogIn.setOnClickListener(new View.OnClickListener() {
