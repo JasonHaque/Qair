5e4ebc9131f7acc72b5a9ae2de925d9314a7d09d
jason <zahinjason220434@gmail.com>
2019-01-22 04:13:44 +0600

    Bottom Navigation to profile fixed

--- app/src/main/java/com/example/jason/qair/Timeline.java
+++ app/src/main/java/com/example/jason/qair/Timeline.java
@@ -3,25 +3,42 @@ package com.example.jason.qair;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.internal.BottomNavigationItemView;
+import android.support.design.widget.BottomNavigationView;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.RadioGroup;
 
 public class Timeline extends Activity {
 
     private ImageButton searchButton,examCreateButton;
+    private BottomNavigationView bottomNav;
+    private BottomNavigationItemView home,activity_log,profile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timeline);
 
+        bottomNav = findViewById(R.id.navigation);
         searchButton = findViewById(R.id.search_button);
         examCreateButton = findViewById(R.id.create_button);
+
+        profile = findViewById(R.id.Profile);
 
         examCreateButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(Timeline.this,ExamCreate.class));
+            }
+        });
+
+        bottomNav.setOnNavigationItemReselectedListener(new BottomNavigationView.OnNavigationItemReselectedListener() {
+            @Override
+            public void onNavigationItemReselected(@NonNull MenuItem menuItem) {
+                startActivity(new Intent(Timeline.this,AccountPage.class));
             }
         });
     }
