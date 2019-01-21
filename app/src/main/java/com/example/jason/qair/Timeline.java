f187fa065736308136f888d617b45eca43606bf2
jason <zahinjason220434@gmail.com>
2019-01-21 17:52:59 +0600

    ExamCreate class added

--- app/src/main/java/com/example/jason/qair/Timeline.java
+++ app/src/main/java/com/example/jason/qair/Timeline.java
@@ -1,12 +1,28 @@
 package com.example.jason.qair;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class Timeline extends Activity {
+
+    private ImageButton searchButton,examCreateButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timeline);
+
+        searchButton = findViewById(R.id.search_button);
+        examCreateButton = findViewById(R.id.create_button);
+
+        examCreateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Timeline.this,ExamCreate.class));
+            }
+        });
     }
 }
