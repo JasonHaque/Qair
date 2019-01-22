984ea982df4cbb30e0feb19818d6101c0bc120be
jason <zahinjason220434@gmail.com>
2019-01-22 23:34:53 +0600

    Bug fixes

--- app/src/main/java/com/example/jason/qair/QuestionCreate.java
+++ app/src/main/java/com/example/jason/qair/QuestionCreate.java
@@ -4,11 +4,12 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 public class QuestionCreate extends Activity {
 
-    private Button questionCreate;
+    private ImageButton questionCreate;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
