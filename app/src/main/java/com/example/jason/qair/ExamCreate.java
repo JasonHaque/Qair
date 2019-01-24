c774ab83e04980a90aad655cfddad683aa8cc311
Pranjal <pknandi130@gmail.com>
2019-01-24 22:47:10 +0600

    Exam create modified

--- app/src/main/java/com/example/jason/qair/ExamCreate.java
+++ app/src/main/java/com/example/jason/qair/ExamCreate.java
@@ -3,21 +3,45 @@ package com.example.jason.qair;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Toast;
 
 public class ExamCreate extends Activity {
 
     private Button CreateExam;
+    private RadioButton mcqOption,writtenOption,publicOption,privateOption;
+    private EditText examName,examDate;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exam_create);
         CreateExam = findViewById(R.id.create_exam);
 
+        mcqOption=findViewById(R.id.mcq_option);
+        writtenOption=findViewById(R.id.written_question_option);
+        publicOption=findViewById(R.id.public_option);
+        privateOption=findViewById(R.id.private_option);
+        examName=findViewById(R.id.exam_name);
+        examDate=findViewById(R.id.Date_option);
+
         CreateExam.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String eName=examName.getText().toString();
+                String eDate=examDate.getText().toString();
+
+                if(TextUtils.isEmpty(eName) || TextUtils.isEmpty(eDate)){
+                    Toast.makeText(ExamCreate.this,"Fill up the fields properly",Toast.LENGTH_LONG).show();
+                    return;
+                }
+                if((!mcqOption.isSelected() && !writtenOption.isSelected()) || (!publicOption.isSelected() && !privateOption.isSelected())){
+                    Toast.makeText(ExamCreate.this,"Fill up the fields properly",Toast.LENGTH_LONG).show();
+                    return;
+                }
                 startActivity(new Intent(ExamCreate.this,QuestionCreate.class));
             }
         });
