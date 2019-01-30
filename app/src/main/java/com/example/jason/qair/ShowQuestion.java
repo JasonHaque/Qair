055ca4819418edd8e340d3f061ea41d7b2e3c23f
jason <zahinjason220434@gmail.com>
2019-01-31 00:43:53 +0600

    Show questions Fixed

--- app/src/main/java/com/example/jason/qair/ShowQuestion.java
+++ app/src/main/java/com/example/jason/qair/ShowQuestion.java
@@ -9,55 +9,53 @@ import android.widget.TextView;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
 public class ShowQuestion extends Activity {
 
-    private Button submitAnswer,NextQuestion;
-    private FirebaseFirestore db;
+
+    private TextView textView;
+    private FirebaseFirestore db=FirebaseFirestore.getInstance();
     private CollectionReference examref = db.collection("exam");
-    private TextView showQuestionNumber,showQuestionText,showOptionA,showOptionB,showOptionC,showOptionD;
-    private EditText Showchoice;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_question);
+        textView = findViewById(R.id.text_view_data);
 
-        showQuestionNumber = findViewById(R.id.Showquestionnumber);
-        showQuestionText = findViewById(R.id.showquestiontext);
-        showOptionA= findViewById(R.id.showoptiona);
-        showOptionB = findViewById(R.id.showoptionb);
-        showOptionC = findViewById(R.id.showoptionc);
-        showOptionD = findViewById(R.id.showoptiond);
-        Showchoice = findViewById(R.id.showchoiceoption);
-        submitAnswer = findViewById(R.id.submit_answer);
-        NextQuestion = findViewById(R.id.next_question);
-
-        int a = Integer.parseInt(showQuestionNumber.getText().toString());
-
-        examref.whereEqualTo("questionnumber",a)
+        examref.orderBy("questionnumber", Query.Direction.ASCENDING)
                 .get()
                 .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
                     @Override
                     public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
-                        for(QueryDocumentSnapshot qshot : queryDocumentSnapshots){
-                            Question question = qshot.toObject(Question.class);
-                            String QuestionText= question.getQuestiontext();
-                            String optiona = question.getOptiona();
-                            String optionb = question.getOptionb();
-                            String optionc = question.getOptionc();
-                            String optiond =question.getOptiond();
+                        String data="";
+                        int count =0;
+                        for (QueryDocumentSnapshot dshot : queryDocumentSnapshots){
+                            Question question = dshot.toObject(Question.class);
 
-                            showQuestionText.setText(QuestionText);
-                            showOptionA.setText(optiona);
-                            showOptionB.setText(optionb);
-                            showOptionC.setText(optionc);
-                            showOptionD.setText(optiond);
+                            int question_number = question.getQuestionnumber();
+                            String question_text =question.getQuestiontext();
+                            String optionA = question.getOptiona();
+                            String optionB = question.getOptionb();
+                            String optionC = question.getOptionc();
+                            String optionD = question.getOptiond();
+                            count++;
+
+                            data += question_number +" "+question_text +"\n"+
+                                    "A" + " "+optionA+"\n"+
+                                    "B" + " "+optionB+"\n"+
+                                    "C" + " "+optionC+"\n"+
+                                    "D" + " "+optionD+"\n\n\n";
+
+                            System.out.println(data);
+
                         }
+                        textView.setText(data);
+                        System.out.println(count);
                     }
                 });
     }
-
 
 }
