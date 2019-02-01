080284c11df6e17ed0f84036db8047cf1f9a4950
Pranjal <pknandi130@gmail.com>
2019-02-01 16:37:13 +0600

    profile layout modified

--- app/src/main/java/com/example/jason/qair/AccountPage.java
+++ app/src/main/java/com/example/jason/qair/AccountPage.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -15,8 +16,8 @@ import static com.example.jason.qair.ApploginPage.userID;
 
 public class AccountPage extends Activity {
 
-    private TextView emailtext;
-    private Button logout;
+    private TextView emailtext,textView;
+    private ImageButton logout;
     private FirebaseAuth firebaseAuth;
 
 
@@ -28,12 +29,14 @@ public class AccountPage extends Activity {
         setContentView(R.layout.activity_account_page);
         firebaseAuth = FirebaseAuth.getInstance();
         emailtext = findViewById(R.id.Textview);
+        textView=findViewById(R.id.TextviewName);
         logout =findViewById(R.id.Logoutbutton);
 
         FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
 
         assert firebaseUser != null;
-        emailtext.setText("Welcome to your account "+ userID);
+        emailtext.setText("Welcome to your account:\n");
+        textView.setText(userID);
 
         logout.setOnClickListener(new View.OnClickListener() {
             @Override
